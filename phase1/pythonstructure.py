11ff4e617fb0fa997ed1535205035e3b4171b124
Harshit-295 <bhardwajharsh1984@gmail.com>
2026-06-19 22:05:36 +0530

    Basic things

---
+++ phase1/pythonstructure.py
@@ -0,0 +1,19 @@
+'''
+_init__.py 
+Makes a folder behave like a package.
+
+
+Type hints 
+def add(a:int,b;int)-> int:
+    return a+b;
+
+
+    Use dataclass:
+
+from dataclasses import dataclass
+
+@dataclass
+class User:
+    name: str
+    age: int
+'''
